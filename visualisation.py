0f7327cc4202ee270d5ddecdb99687c057497d40
Zewen <zewen.liu@postgrad.manchester.ac.uk>
2022-06-27 14:18:11 +0100

    rename files

--- visualisation.py
+++ visualisation.py
@@ -9,7 +9,11 @@ Created on Mon Jun 27 14:08:56 2022
 from model import *
 from model_chapter3 import *
 
-model = torch.load('models/Chapter3_p2a16k9l2_fold4/model')
+# model = torch.load('models/MASC_p2a16k9l2_fold0/model')
+# model = torch.load('models/MASC_p2a16k9l2_fold1/model')
+# model = torch.load('models/MASC_p2a16k9l2_fold2/model')
+# model = torch.load('models/MASC_p2a16k9l2_fold3/model')
+model = torch.load('models/MASC_p2a16k9l2_fold4/model')
 GConv_visual(model)
 convW_visual(model)
 correlation_visual(model)
